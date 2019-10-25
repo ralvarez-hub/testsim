7dee129f27b92dbda57f999a66fa27a3bfce4746
Ruth Álvarez <ruth.alvarez.diaz@gmail.com>
2019-10-25 20:28:35 +0200

    creo que funciona

--- scripts/analyse_sample.sh
+++ scripts/analyse_sample.sh
@@ -1,12 +1,15 @@
-# Defino WD
-WD=$(/home/user16/Desktop/testsim/testsim)
+# defino WD
+WD="/home/user16/Desktop/testsim/testsim"
 cd $WD
 
-# Descargo genoma Ecoli
-mkdir res/genome
-cd $_
-wget -O ecoli.fasta.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
-gunzip -k ecoli.fasta.gz
+if [ "$#" -eq 1 ]
+then
+    sampleid=$1
+
+# descargo genoma Ecoli
+    mkdir res/genome
+    wget -O res/genome/ecoli.fasta.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
+    gunzip -k res/genome/ecoli.fasta.gz
 
 # indexo genoma de Ecoli
     echo "Running STAR index..."
@@ -14,18 +17,14 @@ gunzip -k ecoli.fasta.gz
     STAR --runThreadN 4 --runMode genomeGenerate --genomeDir res/genome/star_index/ --genomeFastaFiles res/genome/ecoli.fasta$
     echo
 
-if [ "$#" -eq 1 ]
-then
-    sampleid=$1
+# proceso secuencias de manera iterativa
+for sampleid in $(ls data/*.fastq.gz | cut -d"_" -f1 | cut -d"/" -f2 | sort |uniq)
+do
     echo "Running FastQC..."
     mkdir -p out/fastqc
     fastqc -o out/fastqc data/${sampleid}*.fastq.gz
     echo
-
-# proceso secuencias de manera iterativa
-for sampleid in $(ls data/*.fastq.gz | cut -d"_" -f1 | cut -d"/" -f2 | sort |uniq)
-do
-echo "Running cutadapt..."
+    echo "Running cutadapt..."
     mkdir -p log/cutadapt
     mkdir -p out/cutadapt
     cutadapt -m 20 -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT -o out/cutadapt/${sampleid}_1.trimmed.fastq.gz -p out/cutadapt/${sampleid}_2.trimmed.fastq.gz data/${sampleid}_1.fastq.gz data/${sampleid}_2.fastq.gz > log/cutadapt/${sampleid}.log
