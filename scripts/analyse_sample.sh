1938843daa1ae1bf10d87f3c6ce6acff88634868
Ruth Álvarez <ruth.alvarez.diaz@gmail.com>
2019-10-30 13:56:06 +0100

    Remove unnecessary if

--- scripts/analyse_sample.sh
+++ scripts/analyse_sample.sh
@@ -2,20 +2,16 @@
 WD="/home/user16/Desktop/testsim/testsim"
 cd $WD
 
-# comprobacion de orden correcta
-if [ "$#" -eq 1 ]
-then
-
 # descargo genoma Ecoli
-    mkdir -p res/genome
-    wget -O res/genome/ecoli.fasta.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
-    gunzip -k res/genome/ecoli.fasta.gz
+mkdir -p res/genome
+wget -O res/genome/ecoli.fasta.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
+gunzip -k res/genome/ecoli.fasta.gz
 
 # indexo genoma de Ecoli
-    echo "Running STAR index..."
-    mkdir -p res/genome/star_index
-    STAR --runThreadN 4 --runMode genomeGenerate --genomeDir res/genome/star_index/ --genomeFastaFiles res/genome/ecoli.fasta
-    echo
+echo "Running STAR index..."
+mkdir -p res/genome/star_index
+STAR --runThreadN 4 --runMode genomeGenerate --genomeDir res/genome/star_index/ --genomeFastaFiles res/genome/ecoli.fasta
+echo
 
 # proceso secuencias de manera iterativa
 for sampleid in $(ls data/*.fastq.gz | cut -d"_" -f1 | cut -d"/" -f2 | sort |uniq)
@@ -37,7 +33,3 @@ done
 
 multiqc -o out/multiqc $WD
 
-else
-    echo "Usage: $0 <sampleid>"
-    exit 1
-fi
